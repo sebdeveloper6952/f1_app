a137522fed2aa3fbf234bbc6ff7a2bdc0e4533cd
Sebastian Arriola <sebdeveloper6952@gmail.com>
2018-06-09 15:28:41 -0600

    Initial commit. Empty project.

---
+++ app/src/main/java/com/projects/sebdeveloper6952/f1_app/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.projects.sebdeveloper6952.f1_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
